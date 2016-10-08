a2204933530787eed6ac1dfa598fdd87be253aef
jpcox <coxjustinp@gmail.com>
2016-10-07 21:22:54 -0600

    Completed calculator program :)

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -1,11 +1,58 @@
-// Calculator.cpp : Defines the entry point for the console application.
-//
+#include <iostream>
+#include<string>
+using namespace std;
 
-#include "stdafx.h"
-
+//program for a calculator type thing
+/******************************************************************
+*INPUT
+*	-2 numbers
+*	-operator
+*
+*OUTPUT
+*	-the result of the 2 numbers
+*
+*******************************************************************/
 
 int main()
 {
+	//variables
+	float num1 = 0, num2 = 0;
+	string mathOperator;
+	float answer = 0;
+
+	//get the 2 numbers
+	cout << "Please enter a number: ";
+	cin >> num1;
+
+	cout << "Please enter another number: ";
+	cin >> num2;
+
+	cout << num1 << " " << num2 << endl;
+	
+	//get the operator
+	cout << "Please enter a math operator(+, -, *, /) : ";
+	cin >> mathOperator;
+
+	//output the result of the 2 numbers
+	if (mathOperator == "+") {
+		answer = num1 + num2;
+	}
+	else if (mathOperator == "-") {
+		answer = num1 - num2;
+	}
+	else if (mathOperator == "*") {
+		answer = num1 * num2;
+	}
+	else if (mathOperator == "/") {
+		answer = num1 / num2;
+	}
+	else {
+		cout << "Please enter a valid math operator: ";
+		cin >> mathOperator;
+	}
+
+	cout << num1 << " " << mathOperator << " " << num2 << " is " << answer << endl;
+	
     return 0;
 }
 
