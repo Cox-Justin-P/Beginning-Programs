8779479deff9c3bfd7c3d8a693c3282fcf4f9a37
jpcox <coxjustinp@gmail.com>
2016-10-13 22:33:06 -0600

    Finished up the Student Score program for now :)

--- Student Scores/Student Scores/Student Scores.cpp
+++ Student Scores/Student Scores/Student Scores.cpp
@@ -9,9 +9,10 @@ using namespace std;
 *	-student scores
 *
 *	OUTPUT
-*	-students names with their scores averaged
+*	-students names with their letter grade
 *
 */
+
 
 int main() {
 	string studentNames[5];
@@ -23,12 +24,45 @@ int main() {
 		i--;
 		cin >> studentNames[i];
 	}
-	
+
 	//output the list of students
 	cout << "Here are the students that you chose: " << endl;
 	for (int i = 0; i < 5; i++) {
 		cout << studentNames[i] << endl;
 	}
+	
+	//have the user enter in the grades
+	cout << "Please enter in the number grade that each student received: " << endl;
+	int numGrades[5];
+	for (int i = 0; i < 5; i++) {
+		cout << studentNames[i] + ": ";
+		cin >> numGrades[i];
+		
+		// get the letter grade
+		char letterGrade;
+		if(numGrades[i] >= 90) {
+			letterGrade = 'A';
+			cout << studentNames[i] << "'s letter grade is: " << letterGrade << endl;
+		}
+		else if (numGrades[i] < 90 && numGrades[i] >= 80) {
+			letterGrade = 'B';
+			cout << studentNames[i] << "'s letter grade is: " << letterGrade << endl;
+		}
+		else if (numGrades[i] < 80 && numGrades[i] >= 70) {
+			letterGrade = 'C';
+			cout << studentNames[i] << "'s letter grade is: " << letterGrade << endl;
+		}
+		else if (numGrades[i] < 70 && numGrades[i] >= 60) {
+			letterGrade = 'D';
+			cout << studentNames[i] << "'s letter grade is: " << letterGrade << endl;
+		}
+		else if (numGrades[i] < 60) {
+			letterGrade = 'F';
+			cout << studentNames[i] << "'s letter grade is: " << letterGrade << endl;
+		}
+	}
+	
+
 
 
 }
